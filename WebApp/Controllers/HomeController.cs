110711bb1d78a916daf2125f9ec87ad3f4c0651a
Michael Haren <mharen@overdrive.com>
2015-04-07 09:33:03 -0400

    initial tomfoolery

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var viewModel = new HomeViewModel();
+            
+            var environmentVariables = Environment.GetEnvironmentVariables();
+            viewModel.EnvironmentVariables = environmentVariables.Keys.OfType<string>()
+                .Select(k=>new KeyValuePair<string, string>(k, environmentVariables[k].ToString()));
+
+            var appSettings = ConfigurationManager.AppSettings;
+            viewModel.AppSettings = appSettings.Keys.OfType<string>()
+                .Select(k => new KeyValuePair<string, string>(k, appSettings[k]));
+
+            var connectionStrings = ConfigurationManager.ConnectionStrings;
+            viewModel.ConnectionStrings = connectionStrings.OfType<string>()
+                .Select(n => new KeyValuePair<string, string>(n, connectionStrings[n].ConnectionString));
+
+            return View(viewModel);
+        }
+
+
+    }
+}
---
+++ WebApp/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<KeyValuePair<string, string>> EnvironmentVariables { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> ConnectionStrings { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> AppSettings { get; set; } 
+    }
+}
